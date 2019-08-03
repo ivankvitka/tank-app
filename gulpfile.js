d5cec35a6dbaefab5a58445e7b8ea84cc8608453
Ivan Kvitka <ivan.kvitka@gmail.com>
2019-08-03 21:34:01 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// Load plugins
+const browsersync = require("browser-sync").create();
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sass = require("gulp-sass");
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./build/"
+    },
+    port: 3000
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// CSS task
+function css() {
+  return gulp
+    .src("./src/scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest("./build/css/"))
+    .pipe(browsersync.stream());
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch("./src/scss/**/*", css);
+  gulp.watch("./build/index.html", browserSyncReload);
+}
+
+// define complex tasks
+const watch = gulp.parallel(watchFiles, browserSync);
+
+// export tasks
+exports.watch = watch;
