bdcdaede7ddcc3812cc9a42b3606bdd2a5b9ae42
Ivan Kvitka <ivan.kvitka@gmail.com>
2019-08-03 22:03:12 +0300

    fixed gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
 // Load plugins
-const browsersync = require("browser-sync").create();
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const sass = require("gulp-sass");
+const browsersync = require('browser-sync').create();
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
 
 // BrowserSync
 function browserSync(done) {
   browsersync.init({
     server: {
-      baseDir: "./build/"
+      baseDir: './build/'
     },
     port: 3000
   });
@@ -26,21 +26,24 @@ function browserSyncReload(done) {
 // CSS task
 function css() {
   return gulp
-    .src("./src/scss/**/*.scss")
+    .src('./src/scss/**/*.scss')
     .pipe(plumber())
-    .pipe(sass({ outputStyle: "expanded" }))
-    .pipe(gulp.dest("./build/css/"))
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(gulp.dest('./build/css/'))
     .pipe(browsersync.stream());
 }
 
 // Watch files
 function watchFiles() {
-  gulp.watch("./src/scss/**/*", css);
-  gulp.watch("./build/index.html", browserSyncReload);
+  gulp.watch('./src/scss/**/*', css);
+}
+
+function html() {
+  gulp.watch('./build/index.html', browserSyncReload);
 }
 
 // define complex tasks
-const watch = gulp.parallel(watchFiles, browserSync);
+const watch = gulp.parallel(watchFiles, browserSync, html);
 
 // export tasks
 exports.watch = watch;
